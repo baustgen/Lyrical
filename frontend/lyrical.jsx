46bf773a56a7d143dca76bc8ffedf637e49a47b4
Braeden Austgen <baustgen2615@gmail.com>
2019-11-05 11:57:22 -0800

    bootstrap currentUser

--- frontend/lyrical.jsx
+++ frontend/lyrical.jsx
@@ -6,11 +6,24 @@ import Root from './components/root';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const store = configureStore();
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+    let store;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
     window.login = login;
     window.logout = logout;
+
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { currentUserId: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore()
+    }
     
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root)
