9150f49ba44ce455c5821a46e967921b0942475b
Braeden Austgen <baustgen2615@gmail.com>
2019-11-04 16:05:21 -0800

    Rename #logout to #logout!

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,17 @@
 class Api::SessionController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[user][username], params[user][password])
+        if @user
+            login(@user)
+            render :show
+        else
+            render json: ['Invalid Username and/or Password'], status: 401
+        end
+    end
+
+    def destroy
+        logout!
+        render json: { message: 'Logout successful.' }
+    end
 end
