34392d8bdcead29c6c7631d93747504121eee26e
Braeden Austgen <baustgen2615@gmail.com>
2019-11-05 12:17:30 -0800

    Add AuthRoute for signup and login

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import SignupFormContainer from "./auth/signup_form_container";
 import LoginFormContainer from "./auth/login_form_container";
 import NavBarContainer from "./nav_bar/nav_bar_container";
+import AuthRoute from "../util/route_util";
 
 const App = () => (
     <div>
@@ -11,8 +12,8 @@ const App = () => (
             <NavBarContainer />
         </header>
 
-        <Route path="/signup" component={SignupFormContainer}/>
-        <Route path="/login" component={LoginFormContainer}/>
+        <AuthRoute path="/signup" component={SignupFormContainer}/>
+        <AuthRoute path="/login" component={LoginFormContainer}/>
     </div>
 );
 
---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { withRouter, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+    <Route
+        path={path}
+        exact={exact}
+        render={props =>
+            loggedIn ? <Redirect to="/"/> : <Component {...props} />
+        }
+    />
+);
+
+const mapStateToProps = state => {
+    return { loggedIn: Boolean(state.session.currentUserId) };
+};
+
+const AuthRoute = withRouter(
+    connect(
+        mapStateToProps,
+        null
+    )(Auth)
+);
+
+export default AuthRoute;
