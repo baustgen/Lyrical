7a7c16c04ab1e0b2aabf4ddf3152ba794d875dc9
Braeden Austgen <baustgen2615@gmail.com>
2019-11-04 17:32:47 -0800

    rename RECEIVE_ERRORS to RECEIVE_SESSION_ERRORS

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionAPIUtil from "../util/session_api_util";
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER"
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS"
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS"
 
 const receiveCurrentUser = (currentUser) => ({
     type: RECEIVE_CURRENT_USER,
@@ -13,8 +13,8 @@ const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER,
 });
 
-const receiveErrors = (errors) => ({
-    type: RECEIVE_ERRORS,
+const receiveSessionErrors = (errors) => ({
+    type: RECEIVE_SESSION_ERRORS,
     errors
 })
 
