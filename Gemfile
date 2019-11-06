a9748d589098fe9eff022a47602da0ac20184012
Braeden Austgen <baustgen2615@gmail.com>
2019-11-06 11:40:49 -0800

    Prepare for initial Heroku push

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'bcrypt'
+gem 'jquery-rails'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
@@ -52,8 +55,6 @@ group :development do
   gem 'binding_of_caller'
   gem 'pry-rails'
   gem 'annotate'
-  gem 'bcrypt'
-  gem 'jquery-rails'
 end
 
 group :test do
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/root_reducer";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 
+const middlewares = [thunk]
+
+if (process.env.NODE_ENV !== "production") {
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+}
 
 const configureStore = (preState = {}) => (
     createStore(rootReducer, preState, 
-        applyMiddleware(thunk, logger))
+        applyMiddleware(...middlewares))
 );
 
 export default configureStore;
