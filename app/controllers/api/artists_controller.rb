f1428561fbe93df55f7d4a9550358422fb87945b
Braeden Austgen <baustgen2615@gmail.com>
2019-11-07 11:12:19 -0800

    Add index and show actions

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,10 +1,12 @@
 class Api::ArtistsController < ApplicationController
 
     def index
-        
+        @artists = Artist.all
+        render :index
     end
 
     def show
-        
+        @artist = Artist.includes(:tracks).find_by(id: params[:id])
+        render :show
     end
 end
