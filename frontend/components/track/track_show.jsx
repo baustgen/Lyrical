ce83e5625ff68de917b58e732c24b2eb5c2309e4
Braeden Austgen <baustgen2615@gmail.com>
2019-11-11 13:13:43 -0800

    Add and style track show page

--- frontend/components/track/track_show.jsx
+++ frontend/components/track/track_show.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 class TrackShow extends React.Component {
@@ -20,23 +21,33 @@ class TrackShow extends React.Component {
 
     render() {
         if (this.props.track === undefined || this.props.artist === undefined) return null;
+
+        let lyrics = this.props.track.lyrics.split("\n").map((line) => {
+            return (
+                <>
+                    <span>{line}</span>
+                    <br/>
+                </>
+            )
+        })
+        
         return (
             <div className="track-show">
                 <div className="track-info-container">
-                    <div className="track-img"></div>
-                    <div>
+                    <div className="track-show-img"></div>
+                    <div className="track-info">
                         <p className="track-title">{this.props.track.title}</p>
-                        <p className="track-artist-name">{this.props.artist.name}</p>
+                        <p className="track-artist"><Link to={`/artists/${this.props.artist.name[0]}/${this.props.track.artistId}`}>{this.props.artist.name}</Link></p>
                         <p className="track-album">{this.props.track.album}</p>
                     </div>
-                    <div className="track-content">
-                        <div className="lyrics-container">
-                            <h4 className="lyrics-header">{this.props.track.title.toUpperCase()}</h4>
-                            <p className="lyrics-text">{this.props.track.lyrics}</p>
-                        </div>
-                        <div className="track-modal">
+                </div>
+                <div className="track-content">
+                    <div className="lyrics-container">
+                        <h4 className="lyrics-header">{this.props.track.title.toUpperCase() + ' LYRICS'}</h4>
+                        <p className="lyrics-text">{lyrics}</p>
+                    </div>
+                    <div className="track-modal">
 
-                        </div>
                     </div>
                 </div>
             </div>
