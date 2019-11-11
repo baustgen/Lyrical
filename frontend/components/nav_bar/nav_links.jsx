d11aac0879b169025a242ec778e84b457f8613df
Braeden Austgen <baustgen2615@gmail.com>
2019-11-11 13:59:17 -0800

    Add Artists link to navbar

--- frontend/components/nav_bar/nav_links.jsx
+++ frontend/components/nav_bar/nav_links.jsx
@@ -4,18 +4,25 @@ import { Link } from "react-router-dom";
 
 const NavLinks = (props) => {
     return (
-        <ul className="nav-links">
-            <li>
-                <a href="https://github.com/baustgen" target="_blank">
-                    <img src="GitHub.png" alt="GitHub"/>
-                </a>
-            </li>
-            <li>
-                <a href="https://www.linkedin.com/in/braeden-austgen-a96b52148/" target="_blank">
-                    <img src="LinkedIn.png" alt="LinkedIn"/>
-                </a>
-            </li>
-        </ul>
+        <div className="header-links-container">
+            <ul className="page-links">
+                <li className="nav-artists-link">
+                    <Link to="/artists">ARTISTS</Link>
+                </li>
+            </ul>
+            <ul className="ext-links">
+                <li>
+                    <a href="https://github.com/baustgen" target="_blank">
+                        <img src="GitHub.png" alt="GitHub"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/braeden-austgen-a96b52148/" target="_blank">
+                        <img src="LinkedIn.png" alt="LinkedIn"/>
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 }
 
