a97acd84955ee9dc3a2a25c56da126b56d475e0b
Braeden Austgen <baustgen2615@gmail.com>
2019-12-11 14:42:29 -0800

    begin styling for splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,36 +5,41 @@ const Splash = () => {
 
     return (
 
-        <main>
+        <div className="splash-container">
             <div className="splash-heading">
                 <h2>Welcome to Lyrical!</h2>
+                <p>Lyrical allows users to explain or add additional information about popular song lyrics.</p> 
+                <p>Simply add an annotation to the lyric for all users to see!</p>
             </div>
 
             <div className="demo-container">
                 <div className="demo-section">
-                    <label>Find a song by artist name
-                        <div className="splash-img-1"></div>
+                    <label>
+                        <img className="splash-img" src="" alt="navigating to song" />
+                        Find a song by artist
                     </label>
                 </div>
                 
                 <div className="demo-section">
-                    <label>Highlight the lyric(s) that you'd like to annotate
-                        <div className="splash-img-2"></div>
+                    <label>
+                        <img className="splash-img" src="" alt="highlighting lyric" />
+                        Highlight the lyric(s) that you'd like to annotate
                     </label>
                 </div>
 
                 <div className="demo-section">
-                    <label>Add your annotation and click 'Create'
-                        <div className="splash-img-3"></div>
+                    <label>
+                        <img className="splash-img" src="" alt="annotation editor" />
+                        Add your annotation and click 'Create'
                     </label>
                 </div>
             </div>
-            <div>
+            <div className="splash-bottom">
                 <p>
                     <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link> to get started!
                 </p>
             </div>
-        </main>
+        </div>
     )
 }
 
