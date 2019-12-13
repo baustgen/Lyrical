baf39f7a880ddb2ef5bbeecc2ba4c7e1402b09ca
Braeden Austgen <baustgen2615@gmail.com>
2019-12-12 16:54:54 -0800

    Add seeds for artists/tracks alpha by artist D - F

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,6 +53,18 @@ darius_rucker = Artist.create({ name: 'Darius Rucker', bio: "Darius Rucker (born
 darius_rucker_img = open('https://lyrical-seeds.s3.amazonaws.com/darius_rucker.jpg')
 darius_rucker.image.attach(io: darius_rucker_img, filename: 'darius_rucker.jpg')
 
+damien_jurado = Artist.create({ name: 'Damien Jurado', bio: "Damien Jurado is a singer-songwriter from Seattle, Washington."})
+damien_jurado_img = open('https://lyrical-seeds.s3.amazonaws.com/damien_jurado.jpg')
+damien_jurado.image.attach(io: damien_jurado_img, filename: 'damien_jurado.jpg')
+
+eminem = Artist.create({ name: 'Eminem', bio: "A hip-hop icon who started as an underground battle rapper in Detroit, Marshall “Eminem” Mathers (1972 – present) has developed a career full of controversy, wild swings, and some of the most noteworthy raps in the history of the genre."})
+eminem_img = open('https://lyrical-seeds.s3.amazonaws.com/eminem.jpg')
+eminem.image.attach(io: eminem_img, filename: 'eminem.jpg')
+
+frnd = Artist.create({ name: 'FRND', bio: "FRND, aka Andrew Goldstein, is a Los Angeles based producer, singer and songwriter that has worked with several artists, such as Demi Lovato, Britney Spears and Krewella. In August 2016, he started the FRND project releasing the single “Friend”. The song gained a lot of unexpected success, leading him to release three more singles and, eventually, his debut EP, “In Your Dreams”, in 2017."})
+frnd_img = open('https://lyrical-seeds.s3.amazonaws.com/frnd.jpg')
+frnd.image.attach(io: frnd_img, filename: 'frnd.jpg')
+
 
 # END ARTISTS
 
@@ -1338,8 +1350,8 @@ Dandelion",
 same_drugs_img = open('https://lyrical-seeds.s3.amazonaws.com/same_drugs.jpg')
 same_drugs.image.attach(io: same_drugs_img, filename: 'same_drugs.jpg')
 
-wagon_wheel = Track.create({ title: "Same Drugs",
-    album: "Coloring Book",
+wagon_wheel = Track.create({ title: "Wagon Wheel",
+    album: "True Believers",
     lyrics: "[Verse 1]
 Heading down south to the land of the pines
 I'm thumbing my way into North Caroline
@@ -1402,6 +1414,393 @@ Rock me",
 })
 wagon_wheel_img = open('https://lyrical-seeds.s3.amazonaws.com/wagon_wheel.jpg')
 wagon_wheel.image.attach(io: wagon_wheel_img, filename: 'wagon_wheel.jpg')
+
+sheets = Track.create({ title: "Sheets",
+    album: "Caught in the Trees",
+    lyrics: "Cause he's still coming around like an injured bird needing a nest
+A place to rest his head in a song you'll regret
+Still you take him, Lord knows I don't want to compete
+Still I sleep in the very sheets he's been in
+
+Swallow him whole like a pill that makes you choke, it steals your soul
+You have the nerve to look me in the eyes and lie
+Send him back, I won't share the trap that you have me in
+
+Is he still coming around like an injured bird needing a nest
+A place to rest his head in a song you'll regret
+Still you take him, Lord knows I don't want to compete
+Still I sleep in the very sheets he's been in
+
+Swallow him whole like a pill that makes you choke, steals your soul
+You have the nerve to look me in the eyes and lie
+Send him back, I won't share the trap that you have me in
+
+Still I sleep in the very sheets he's been in",
+    artist_id: damien_jurado.id
+})
+sheets_img = open('https://lyrical-seeds.s3.amazonaws.com/sheets.jpg')
+sheets.image.attach(io: sheets_img, filename: 'sheets.jpg')
+
+lose_yourself = Track.create({ title: "Lose Yourself",
+    album: "The Singles",
+    lyrics: "[Intro]
+Look, if you had one shot, or one opportunity
+To seize everything you ever wanted, in one moment
+Would you capture it, or just let it slip?
+
+[Verse 1]
+Yo! His palms are sweaty, knees weak, arms are heavy
+There's vomit on his sweater already: Mom's spaghetti
+He's nervous, but on the surface he looks calm and ready
+To drop bombs, but he keeps on forgetting
+What he wrote down, the whole crowd goes so loud
+He opens his mouth, but the words won't come out
+He's choking, how? Everybody's joking now
+The clock's run out, time's up, over—blaow!
+Snap back to reality, ope there goes gravity, ope
+There goes Rabbit, he choked, he's so mad but he won't
+Give up that easy, no, he won't have it, he knows
+His whole back's to these ropes, it don't matter, he's dope
+He knows that but he's broke, he's so stagnant, he knows
+When he goes back to this mobile home, that's when it's
+Back to the lab again yo, this whole rhapsody
+Better go capture this moment and hope it don't pass him, and
+
+[Chorus]
+You better lose yourself in the music
+The moment, you own it, you better never let it go (Go!)
+You only get one shot, do not miss your chance to blow
+This opportunity comes once in a lifetime, yo
+You better lose yourself in the music
+The moment, you own it, you better never let it go (Go!)
+You only get one shot, do not miss your chance to blow
+This opportunity comes once in a lifetime, yo
+You better…
+
+[Verse 2]
+His soul's escaping through this hole that is gaping
+This world is mine for the taking, make me king
+As we move toward a New World Order
+A normal life is boring; but superstardom's
+Close to post-mortem, it only grows harder
+Homie grows hotter, he blows, it's all over
+These hoes is all on him, coast-to-coast shows
+He's known as the Globetrotter, lonely roads
+God only knows, he's grown farther from home, he's no father
+He goes home and barely knows his own daughter
+But hold your nose, 'cause here goes the cold water
+These hoes don't want him no mo', he's cold product
+They moved on to the next schmoe who flows
+He nose-dove and sold nada, and so the soap opera
+Is told, it unfolds, I suppose it's old, partner
+But the beat goes on: da-da-dom, da-dom, dah-dah, dah-dah
+
+[Chorus]
+You better lose yourself in the music
+The moment, you own it, you better never let it go (Go!)
+You only get one shot, do not miss your chance to blow
+This opportunity comes once in a lifetime, yo
+You better lose yourself in the music
+The moment, you own it, you better never let it go (Go!)
+You only get one shot, do not miss your chance to blow
+This opportunity comes once in a lifetime, yo
+You better…
+
+[Verse 3]
+No more games, I'ma change what you call rage
+Tear this motherfuckin' roof off like two dogs caged
+I was playin' in the beginning, the mood all changed
+I've been chewed up and spit out and booed off stage
+But I kept rhymin' and stepped right in the next cypher
+Best believe somebody's payin' the Pied Piper
+All the pain inside amplified by the
+Fact that I can't get by with my nine-to-
+Five and I can't provide the right type of life for my family
+'Cause man, these goddamn food stamps don't buy diapers
+And there's no movie, there's no Mekhi Phifer, this is my life
+And these times are so hard, and it's gettin' even harder
+Tryna feed and water my seed, plus teeter-totter
+Caught up between bein' a father and a prima donna
+Baby mama drama, screamin' on her, too much for me to wanna
+Stay in one spot, another day of monotony's
+Gotten me to the point I'm like a snail, I've got
+To formulate a plot or end up in jail or shot
+Success is my only motherfuckin' option—failure's not
+Mom, I love you, but this trailer's got
+To go; I cannot grow old in Salem's Lot
+So here I go, it's my shot: feet, fail me not
+This may be the only opportunity that I got
+
+[Chorus]
+You better lose yourself in the music
+The moment, you own it, you better never let it go (Go!)
+You only get one shot, do not miss your chance to blow
+This opportunity comes once in a lifetime, yo
+You better lose yourself in the music
+The moment, you own it, you better never let it go (Go!)
+You only get one shot, do not miss your chance to blow
+This opportunity comes once in a lifetime, yo
+You better…
+
+[Outro]
+You can do anything you set your mind to, man",
+    artist_id: eminem.id
+})
+lose_yourself_img = open('https://lyrical-seeds.s3.amazonaws.com/lose_yourself.png')
+lose_yourself.image.attach(io: lose_yourself_img, filename: 'lose_yourself.png')
+
+rap_god = Track.create({ title: "Rap God",
+    album: "The Marshall Mathers LP2",
+    lyrics: "[Intro]
+\"Look, I was gonna go easy on you not to hurt your feelings.\"
+\"But I'm only going to get this one chance.\"
+\"Something's wrong, I can feel it.\"
+Six minutes. Six minutes. Six minutes, Slim Shady, you're on!
+\"Just a feeling I've got. Like something's about to happen, but I don't know what. If that means what I think it means, we're in trouble, big trouble; and if he is as bananas as you say, I'm not taking any chances.\"
+\"You are just what the doc ordered.\"
+
+[Chorus]
+I'm beginnin' to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+Now, who thinks their arms are long enough to slap box, slap box?
+They said I rap like a robot, so call me Rap-bot
+
+[Verse 1]
+But for me to rap like a computer it must be in my genes
+I got a laptop in my back pocket
+My pen'll go off when I half-cock it
+Got a fat knot from that rap profit
+Made a livin' and a killin' off it
+Ever since Bill Clinton was still in office
+With Monica Lewinsky feelin' on his nutsack
+I'm an MC still as honest
+But as rude and as indecent as all hell
+Syllables, skill-a-holic (Kill 'em all with)
+This flippity dippity-hippity hip-hop
+You don't really wanna get into a pissin' match
+With this rappity brat, packin' a MAC in the back of the Ac'
+Backpack rap crap, yap-yap, yackety-yack
+And at the exact same time, I attempt these lyrical acrobat stunts while I'm practicin' that
+I'll still be able to break a motherfuckin' table
+Over the back of a couple of faggots and crack it in half
+Only realized it was ironic, I was signed to Aftermath after the fact
+How could I not blow? All I do is drop F-bombs
+Feel my wrath of attack
+Rappers are havin' a rough time period, here's a maxi pad
+It's actually disastrously bad for the wack
+While I'm masterfully constructing this masterpiece as
+
+[Chorus]
+'Cause I'm beginnin' to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+Now, who thinks their arms are long enough to slap box, slap box?
+Let me show you maintainin' this shit ain't that hard, that hard
+Everybody wants the key and the secret to rap immortality like Ι have got
+
+[Verse 2]
+Well, to be truthful the blueprint's
+Simply rage and youthful exuberance
+Everybody loves to root for a nuisance
+Hit the Earth like an asteroid
+Did nothing but shoot for the Moon since (Pew!)
+MCs get taken to school with this music
+'Cause I use it as a vehicle to \"bus the rhyme\"
+Now I lead a new school full of students
+Me? I'm a product of Rakim
+Lakim Shabazz, 2Pac, N.W.A, Cube, hey Doc, Ren
+Yella, Eazy, thank you, they got Slim
+Inspired enough to one day grow up, blow up and be in a position
+To meet Run–D.M.C., induct them
+Into the motherfuckin' Rock and Roll Hall of Fame
+Even though I'll walk in the church and burst in a ball of flames
+Only Hall of Fame I'll be inducted in is the alcohol of fame
+On the wall of shame
+You fags think it's all a game, 'til I walk a flock of flames
+Off a plank and, tell me what in the fuck are you thinkin'?
+Little gay-lookin' boy
+So gay I can barely say it with a straight face, lookin' boy (Ha-ha!)
+You're witnessin' a mass-occur
+Like you're watching a church gathering take place, lookin' boy
+\"Oy vey, that boy's gay!\"—that's all they say, lookin' boy
+You get a thumbs up, pat on the back
+And a \"way to go\" from your label every day, lookin' boy
+Hey, lookin' boy! What you say, lookin' boy?
+I get a \"hell yeah\" from Dre, lookin' boy
+I'ma work for everything I have, never asked nobody for shit
+Get outta my face, lookin' boy!
+Basically, boy, you're never gonna be capable
+Of keepin' up with the same pace, lookin' boy, 'cause—
+
+[Chorus]
+I'm beginnin' to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+The way I'm racin' around the track, call me NASCAR, NASCAR
+Dale Earnhardt of the trailer park, the White Trash God
+Kneel before General Zod
+This planet's Krypton—no, Asgard, Asgard
+
+[Verse 3]
+So you'll be Thor, I'll be Odin
+You rodent, I'm omnipotent
+Let off, then I'm reloadin'
+Immediately with these bombs I'm totin'
+And I should not be woken
+I'm the walkin' dead, but I'm just a talkin' head, a zombie floatin'
+But I got your mom deep-throatin'
+I'm out my Ramen Noodle
+We have nothin' in common, poodle
+I'm a Doberman, pinch yourself in the arm and pay homage, pupil
+It's me, my honesty's brutal
+But it's honestly futile if I don't utilize what I do though
+For good at least once in a while
+So I wanna make sure somewhere in this chicken scratch I scribble and doodle enough rhymes
+To maybe try to help get some people through tough times
+But I gotta keep a few punchlines
+Just in case 'cause even you unsigned
+Rappers are hungry lookin' at me like it's lunchtime
+I know there was a time where once I
+Was king of the underground
+But I still rap like I'm on my Pharoahe Monch grind
+So I crunch rhymes, but sometimes when you combine
+Appeal with the skin color of mine
+You get too big and here they come tryin'
+To censor you like that one line
+I said on \"I'm Back\" from The Mathers LP 1 when I
+Tried to say I'll take seven kids from Columbine
+Put 'em all in a line, add an AK-47, a revolver and a 9
+See if I get away with it now that I ain't as big as I was, but I'm
+Morphin' into an immortal, comin' through the portal
+You're stuck in a time warp from 2004 though
+And I don't know what the fuck that you rhyme for
+You're pointless as Rapunzel with fuckin' cornrows
+You write normal? Fuck being normal!
+And I just bought a new raygun from the future
+Just to come and shoot ya, like when Fabolous made Ray J mad
+'Cause Fab said he looked like a fag at Mayweather's pad
+Singin' to a man while he played piano
+Man, oh man, that was a 24/7 special on the cable channel
+So Ray J went straight to the radio station
+The very next day, \"Hey Fab, I'ma kill you!\"
+Lyrics comin' at you at supersonic speed (J.J. Fad)
+Uh, summa-lumma, dooma-lumma, you assumin' I'm a human
+What I gotta do to get it through to you I'm superhuman?
+Innovative and I'm made of rubber so that anything you say is ricochetin' off of me and it'll glue to you and
+I'm devastating, more than ever demonstrating
+How to give a motherfuckin' audience a feeling like it's levitating
+Never fading, and I know the haters are forever waiting
+For the day that they can say I fell off, they'll be celebrating
+'Cause I know the way to get 'em motivated
+I make elevating music, you make elevator music
+\"Oh, he's too mainstream.\"
+Well, that's what they do when they get jealous, they confuse it
+\"It's not hip-hop, it's pop,\"—'cause I found a hella way to fuse it
+With rock, shock rap with Doc
+Throw on \"Lose Yourself\" and make 'em lose it
+\"I don't know how to make songs like that
+I don't know what words to use.\"
+Let me know when it occurs to you
+While I'm rippin' any one of these verses that versus you
+It's curtains, I'm inadvertently hurtin' you
+How many verses I gotta murder to
+Prove that if you were half as nice, your songs you could sacrifice virgins too?!
+Ugh, school flunky, pill junkie
+But look at the accolades these skills brung me
+Full of myself, but still hungry
+I bully myself 'cause I make me do what I put my mind to
+And I'm a million leagues above you
+Ill when I speak in tongues, but it's still tongue-in-cheek, fuck you
+I'm drunk, so, Satan, take the fucking wheel
+I'ma sleep in the front seat
+Bumpin' Heavy D and the Boyz, still \"Chunky but Funky\"
+But in my head there's something I can feel tugging and struggling
+Angels fight with devils and here's what they want from me
+They're askin' me to eliminate some of the women hate
+But if you take into consideration the bitter hatred
+I have, then you may be a little patient
+And more sympathetic to the situation
+And understand the discrimination
+But fuck it, life's handin' you lemons? Make lemonade then!
+But if I can't batter the women
+How the fuck am I supposed to bake them a cake then?!
+Don't mistake him for Satan; it's a fatal mistake
+If you think I need to be overseas and take a vacation
+To trip a broad, and make her fall on her face and
+Don't be a retard—be a king? Think not
+Why be a king when you can be a God?",
+    artist_id: eminem.id
+})
+rap_god_img = open('https://lyrical-seeds.s3.amazonaws.com/rap_god.png')
+rap_god.image.attach(io: rap_god_img, filename: 'rap_god.png')
+
+sticks_and_stones = Track.create({ title: "Sticks and Stones",
+    album: "In Your Dreams",
+    lyrics: "[Intro]
+Sticks and stones may break my bones
+But you could never hurt me
+Sticks and stones may break my bones
+But you could never hurt me
+
+[Verse 1]
+This is the part when I shut you out
+Trying so hard to shut my mouth
+Why do I feel drunk with love
+You're right, it's time to pull the plug
+
+[Pre-Chorus]
+If they never get close to you
+Then it's never gonna hurt
+If they never get close to you
+(Get close to you, get close to you)
+
+[Chorus]
+Sticks and stones may break my bones
+But you could never hurt me
+Sticks and stones may break my bones
+But you could never hurt me
+How do you stay beautiful in this ugly world
+Sticks and stones may break my bones
+But you know how to kill me, girl
+
+[Verse 2]
+This is the part when I let you go
+Put these walls up
+They fall like dominoes
+I just smile and pretend
+Why can't I be fine as friends
+
+[Pre-Chorus]
+If they never get close to you
+Then it's never gonna hurt
+If they never get close to you
+(Get close to you, get close to you)
+
+[Chorus]
+Sticks and stones may break my bones
+But you could never hurt me
+Sticks and stones may break my bones
+But you could never hurt me
+How do you stay beautiful in this ugly world
+Sticks and stones may break my bones
+But you know how to kill me, girl
+
+[Pre-Chorus]
+If they never get close to you
+Then it's never gonna hurt
+If they never get close to you
+(Get close to you, get close to you)
+
+[Chorus]
+Sticks and stones may break my bones
+But you could never hurt me
+Sticks and stones may break my bones
+But you could never hurt me
+How do you stay beautiful in this ugly world
+Sticks and stones may break my bones
+But you know how to kill me, girl",
+    artist_id: eminem.id
+})
+sticks_and_stones_img = open('https://lyrical-seeds.s3.amazonaws.com/sticks_and_stones.jpg')
+sticks_and_stones.image.attach(io: sticks_and_stones_img, filename: 'sticks_and_stones.jpg')
 
 
 # END TRACKS
