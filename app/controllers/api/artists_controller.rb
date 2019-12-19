bd3307d3d2c7d0718dc9968d471c35116da28d72
Braeden Austgen <baustgen2615@gmail.com>
2019-12-18 16:49:27 -0800

    fix n+1 query for artist images

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -3,9 +3,20 @@ class Api::ArtistsController < ApplicationController
     def index
         alpha = ("a".."z").to_a + ("A".."Z").to_a
         if (params[:letter] == 'num')
-            @artists = Artist.where(name: "0%".."9%").includes(:image).order(:name)
+            @artists = Artist.where("name ILIKE ?", "0%")
+                .or(Artist.where("name ILIKE ?", "1%"))
+                .or(Artist.where("name ILIKE ?", "2%"))
+                .or(Artist.where("name ILIKE ?", "3%"))
+                .or(Artist.where("name ILIKE ?", "4%"))
+                .or(Artist.where("name ILIKE ?", "5%"))
+                .or(Artist.where("name ILIKE ?", "6%"))
+                .or(Artist.where("name ILIKE ?", "7%"))
+                .or(Artist.where("name ILIKE ?", "8%"))
+                .or(Artist.where("name ILIKE ?", "9%"))
+                .order(:name)
+                .with_attached_image
         else
-            @artists = Artist.where("name ILIKE ?", "#{params[:letter]}%").order(:name)
+            @artists = Artist.where("name ILIKE ?", "#{params[:letter]}%").with_attached_image.order(:name)
         end
         render :index
     end
