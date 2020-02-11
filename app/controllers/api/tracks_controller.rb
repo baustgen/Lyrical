c030b4ac29a1c391d11969d7d6ec5309a7f9c7c1
Braeden Austgen <baustgen2615@gmail.com>
2020-02-11 09:58:54 -0800

    add Tracks#search action to match track titles

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -9,4 +9,9 @@ class Api::TracksController < ApplicationController
         @track = Track.includes(:artist).includes(:annotations).with_attached_image.find_by(id: params[:id])
         render :show
     end
+
+    def search
+        @tracks = Track.includes(:artist).with_attached_image.where('title ILIKE ?', '%' + params["q"] + '%')
+        render :index
+    end
 end
