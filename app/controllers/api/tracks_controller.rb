3ef9dbeba0dd90fc9ff6f7af751b1ab32f26c939
Braeden Austgen <baustgen2615@gmail.com>
2020-02-11 10:00:34 -0800

    remove unused tracks

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,10 +1,5 @@
 class Api::TracksController < ApplicationController
 
-    def index
-        @tracks = Track.all
-        render :index
-    end
-
     def show
         @track = Track.includes(:artist).includes(:annotations).with_attached_image.find_by(id: params[:id])
         render :show
